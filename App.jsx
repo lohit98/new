d40fe9166fc3525eb5be080e8b59c771c6ad565a
lohit98 <chandra.lohit98@gmail.com>
2020-10-27 16:58:48 +0530

    gi

---
+++ App.jsx
@@ -0,0 +1,18 @@
+
+import React from "react"
+class App extends React.Component{
+    render(){
+        return(
+             <h1>
+                123
+            </h1>
+               
+            
+        );
+    }
+}
+
+export default App;
+
+
+
---
+++ App1.jsx
@@ -0,0 +1,18 @@
+
+import React from "react"
+class App1 extends React.Component{
+    render(){
+        return(
+            <h1>
+                lohit
+            </h1>
+               
+            
+        );
+    }
+}
+
+export default App1;
+
+
+
